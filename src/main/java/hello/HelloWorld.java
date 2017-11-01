4536863ff595edbbf436fd26f9a02875d7e443f1
steven <tly@codein.tv>
2017-11-01 10:45:44 +0800

    gradle 4.3

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -7,21 +7,22 @@ public class HelloWorld {
     LocalTime currentTime = new LocalTime();
     System.out.println("The current local time is: " + currentTime);
 
-    //Greeter greeter = new Greeter();
-    //System.out.println(greeter.sayHello());
+    Greeter greeter = new Greeter();
+    System.out.println(greeter.sayHello());
 
     try {
       OkPost example = new OkPost();
       String json = example.bowlingJson2("Jesse", "Jake");
-      //String response = example.post("http://www.roundsapp.com/post", json);
-      String url = "https://gw.codein.net/echo";
-      System.out.println("url: " + url + " method: POST body: " + json);
-      String response = example.post(url, json);
+      String response = example.post("http://www.roundsapp.com/post", json);
+      //String url = "https://gw.codein.net/echo";
+      //System.out.println("url: " + url + " method: POST body: " + json);
+      //String response = example.post(url, json);
       System.out.println("resp: " + response);
 
     } catch(Exception e) {
       System.out.println(e);
     }
+
     System.out.println("exiting ...");
   }
 }
